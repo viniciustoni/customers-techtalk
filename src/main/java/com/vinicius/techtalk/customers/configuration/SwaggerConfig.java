43149004690a6b4b2b915999b742dd3f739a07c3
vinicius.a.gai <vinicius.a.gai@KRK1-LDL-P90506.synapse.com>
2019-11-19 17:51:39 +0100

    Downgrade springboot for integrity with swagger

--- src/main/java/com/vinicius/techtalk/customers/configuration/SwaggerConfig.java
+++ src/main/java/com/vinicius/techtalk/customers/configuration/SwaggerConfig.java
@@ -1,67 +1,48 @@
 package com.vinicius.techtalk.customers.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-//@EnableSwagger2
+@EnableSwagger2
 public class SwaggerConfig {
-//
-//    @Bean
-//    public LinkDiscoverers discoverers() {
-//        List<JsonPathLinkDiscoverer> plugins = new ArrayList<>();
-//        plugins.add(new HalLinkDiscoverer());
-//        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-//    }
-//
-//    @Bean
-//    public LinkRelationProvider provider() {
-//        return new EvoInflectorLinkRelationProvider();
-//    }
-//    
-//    @Bean
-//    @Primary
-//    public PluginRegistryFactoryBean<LinkRelationProvider, LinkRelationProvider.LookupContext>
-//        myPluginRegistryProvider(){
-//        
-//        PluginRegistryFactoryBean<LinkRelationProvider, LinkRelationProvider.LookupContext> factory 
-//            = new PluginRegistryFactoryBean<>();
-//        
-//        factory.setType(LinkRelationProvider.class);
-//        Class<?> classes[] = new Class<?>[1]; 
-//        classes[0] = DelegatingLinkRelationProvider.class;
-//        factory.setExclusions(classes);
-//        
-//        return factory;
-//    }
-//
-//    @Bean
-//    public Docket api() {
-//
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("customer-api")
-//                .select()
-//                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot"))).build()
-//                .apiInfo(apiInfo()).tags(new Tag("Customer Techtalk", "Customer Techtalk"))
-//                .globalResponseMessage(RequestMethod.GET, responseMessages());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("Customer")
-//                .description("Customer - TechTalks")
-//                .version("v1")
-//                .contact(new Contact("Vinicius Antonio Gai.", "https://github.com/viniciustoni/", "viniciustoni@gmail.com"))
-//                .build();
-//    }
-//
-//    private List<ResponseMessage> responseMessages() {
-//        final List<ResponseMessage> responseMessages = new ArrayList<>();
-//
-//        responseMessages.add(new ResponseMessageBuilder().code(200).message("Success").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(401).message("Unauthorized").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(403).message("Forbidden").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(404).message("Not Found").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(500).message("Failure").build());
-//
-//        return responseMessages;
-//    }
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2).groupName("customer-api").select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot"))).build()
+                .apiInfo(apiInfo()).globalResponseMessage(RequestMethod.GET, responseMessages());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Customer").description("Customer - TechTalks").version("v1").build();
+    }
+
+    private List<ResponseMessage> responseMessages() {
+        final List<ResponseMessage> responseMessages = new ArrayList<>();
+
+        responseMessages.add(new ResponseMessageBuilder().code(200).message("Success").build());
+        responseMessages.add(new ResponseMessageBuilder().code(401).message("Unauthorized").build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message("Forbidden").build());
+        responseMessages.add(new ResponseMessageBuilder().code(404).message("Not Found").build());
+        responseMessages.add(new ResponseMessageBuilder().code(500).message("Failure").build());
+
+        return responseMessages;
+    }
 }
